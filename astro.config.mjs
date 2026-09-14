32fe4102ed9ac824e3eb75fd2d80204d86c6864c
casey-w-coffman <3218research@gmail.com>
2026-09-13 21:39:01 -0500

    Add deployment files

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,8 @@ import { defineConfig, fontProviders } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://3218research.github.io',
+	base: '/3218Website',
 	integrations: [mdx(), sitemap()],
 	fonts: [
 		{
